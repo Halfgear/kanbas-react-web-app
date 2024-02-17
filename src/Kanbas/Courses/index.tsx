67fe4cf53ec9c0dee26f45a0d10dc1ecbd286020
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-02-17 15:11:02 -0500

    :finished 3.4

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,13 +1,35 @@
 import { courses } from "../../Kanbas/Database";
-import { useParams } from "react-router-dom";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
+import CourseNavigation from "./Navigation";
 import "./index.css";
 function Courses() {
     const { courseId } = useParams();
+    const location = useLocation();
+    const currentPath = location.pathname.split('/').filter(Boolean).pop()
     const course = courses.find((course) => course._id === courseId);
+
     return (
         <div className="courses-container">
-            <h1><HiMiniBars3 className="react-icon" />  {course?._id} {course?.name}</h1>
+            <h1><HiMiniBars3 className="react-icon" />  {course?._id} {course?.name}  {">"}
+                <span className="path">{currentPath}</span></h1>
+            <hr />
+            <CourseNavigation />
+            <div>
+                <div
+                    className="overflow-y-scroll position-fixed bottom-0 end-0 color"
+                    style={{ left: "320px", top: "70px", color: "black", fontWeight: "bold" }} >
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<h1 className="tap-title">Home</h1>} />
+                        <Route path="Modules" element={<h1 className="tap-title">Modules</h1>} />
+                        <Route path="Piazza" element={<h1 className="tap-title">Piazza</h1>} />
+                        <Route path="Assignments" element={<h1 className="tap-title">Assignments</h1>} />
+                        <Route path="Assignments/:assignmentId" element={<h1 className="tap-title">Assignment Editor</h1>} />
+                        <Route path="Grades" element={<h1 className="tap-title">Grades</h1>} />
+                    </Routes>
+                </div>
+            </div>
         </div>
     );
 }
