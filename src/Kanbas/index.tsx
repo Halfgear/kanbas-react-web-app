6c5d57c4f8262df16af5e47fd6fb6b23986574dd
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-03-13 13:47:27 -0400

    finished 3.3

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,6 +5,8 @@ import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import * as db from "./Database";
 import { useState } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>(db.courses);
@@ -32,26 +34,28 @@ function Kanbas() {
   };
 
   return (
-    <div className="d-flex">
-      <KanbasNavigation />
-      <div style={{ flexGrow: 1 }}>
-        <Routes>
-          <Route path="/" element={<Navigate to="Dashboard" />} />
-          <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={<Dashboard
-            courses={courses}
-            course={course}
-            setCourse={setCourse}
-            addNewCourse={addNewCourse}
-            deleteCourse={deleteCourse}
-            updateCourse={updateCourse} />
-          } />
-          <Route path="Courses/*" element={<h1>Courses</h1>} />
-          <Route path="Courses/:courseId/*" element={<Courses courses={courses} />} />
-        </Routes>
+    <Provider store={store}>
+      <div className="d-flex">
+        <KanbasNavigation />
+        <div style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Dashboard" element={<Dashboard
+              courses={courses}
+              course={course}
+              setCourse={setCourse}
+              addNewCourse={addNewCourse}
+              deleteCourse={deleteCourse}
+              updateCourse={updateCourse} />
+            } />
+            <Route path="Courses/*" element={<h1>Courses</h1>} />
+            <Route path="Courses/:courseId/*" element={<Courses courses={courses} />} />
+          </Routes>
 
+        </div>
       </div>
-    </div>
+    </Provider>
   )
 }
 export default Kanbas;
