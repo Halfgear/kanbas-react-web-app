fc21ca9bab7b783c597e6448fc5de4fd1b251f40
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-03-25 21:27:24 -0400

    finished 3.4

--- src/Labs/a5/WorkingWithArrays.tsx
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function WorkingWithArrays() {
     const [todo, setTodo] = useState({
@@ -8,12 +9,48 @@ function WorkingWithArrays() {
         due: "2021-09-09",
         completed: false,
     });
+    const [todos, setTodos] = useState([]);
+    const fetchTodos = async () => {
+        const response = await axios.get(API);
+        setTodos(response.data);
+    };
+    const removeTodo = async (todo: { id: any; }) => {
+        const response = await axios
+            .get(`${API}/${todo.id}/delete`);
+        setTodos(response.data);
+    };
+    const createTodo = async () => {
+        const response = await axios.get(`${API}/create`);
+        setTodos(response.data);
+    };
+    const fetchTodoById = async (id: any) => {
+        const response = await axios.get(`${API}/${id}`);
+        setTodo(response.data);
+    };
+
+    const updateTitle = async () => {
+        const response = await axios.get(`${API}/${todo.id}/title/${todo.title}`);
+        setTodos(response.data);
+    };
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
 
     const API = "http://localhost:4000/a5/todos";
     return (
         <div>
             <hr />
             <h2>Working with Arrays</h2>
+            <button className='btn btn-primary' onClick={createTodo} >
+                Create Todo
+            </button>
+            <br />
+            <button className = 'btn btn-success' onClick={updateTitle} >
+                Update Title
+            </button>
+            <br />
+            <br />
             <input type="number" value={todo.id}
                 onChange={(e) => setTodo({
                     ...todo, id: parseInt(e.target.value)
@@ -22,6 +59,26 @@ function WorkingWithArrays() {
                 onChange={(e) => setTodo({
                     ...todo, title: e.target.value
                 })} />
+            <br /><br />
+            <ul>
+                {todos.map((todo: any) => (
+                    <li key={todo.id} className="list-group-item">
+                        {todo.title}
+                        <button
+                            style={{ marginLeft: '20px', marginRight: '10px' }}
+                            className='btn btn-danger'
+                            onClick={() => removeTodo(todo)} >
+                            Remove
+                        </button>
+                        <button
+                            className='btn btn-warning'
+                            onClick={() => fetchTodoById(todo.id)} >
+                            Edit
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
             <h3>Updating an Item in an Array</h3>
             <a className='btn btn-primary' href={`${API}/${todo.id}/title/${todo.title}`} >
                 Update Title to {todo.title}
--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function WorkingWithObjects() {
     const [assignment, setAssignment] = useState({
         id: 1, title: "NodeJS Assignment",
@@ -10,11 +11,35 @@ function WorkingWithObjects() {
         description: "Create a NodeJS module", completed: false, score: 0,
     });
     const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment"
+    const fetchAssignment = async () => {
+        const response = await axios.get(`${ASSIGNMENT_URL}`);
+        setAssignment(response.data);
+    };
+    const updateTitle = async () => {
+        const response = await axios
+            .get(`${ASSIGNMENT_URL}/title/${assignment.title}`);
+        setAssignment(response.data);
+    };
+    useEffect(() => {
+        fetchAssignment();
+    }, []);
+
     const MODULE_URL = "http://localhost:4000/a5/module"
     return (
         <div>
             <hr />
             <h3>Working With Objects</h3>
+            <h3>Modifying Properties</h3>
+            <input onChange={(e) => setAssignment({
+                ...assignment, title: e.target.value
+            })}
+                value={assignment.title} type="text" />
+            <button onClick={updateTitle} >
+                Update Title to: {assignment.title}
+            </button>
+            <button onClick={fetchAssignment} >
+                Fetch Assignment
+            </button>
             <h4> On my own section</h4>
             <a className="btn btn-primary"
                 href={`http://localhost:4000/a5/module`}>
@@ -43,7 +68,7 @@ function WorkingWithObjects() {
             </a>
             <br />
             <input className="form-check-input"
-            type="checkbox"
+                type="checkbox"
                 onChange={(e) => setModule({
                     ...module,
                     completed: e.target.checked
