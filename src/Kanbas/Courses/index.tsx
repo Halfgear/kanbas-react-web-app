c446ec7d390612791318837539395288ddffaaca
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-04-02 00:45:14 -0400

    bug fixed

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -11,7 +11,8 @@ import axios from "axios";
 function Courses() {
     const { courseId } = useParams();
     const location = useLocation();
-    const COURSES_API = "http://localhost:4000/api/courses";
+    const API_BASE = process.env.REACT_APP_API_BASE;
+    const COURSES_API = `${API_BASE}/api/courses`;
     const [course, setCourse] = useState<any>({ _id: "" });
     const findCourseById = async (courseId?: string) => {
         const response = await axios.get(
